3f2c6ec83382d623eddee2638cbf9b248db9c690
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-05-10 10:12:06 -0700

    anaconda -> conda and better test for Docker container

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 # NOTE: this script tests both Docker and singularity, but the data is mounted on different folders
 FAILED=0
-if [ -d /data/example_data ]; then
+if [ -d /data/example_data ] || [ -f /.dockerenv ]; then
   CONTAINER_TYPE=docker
   DATA_ROOT=/data/
 else
@@ -12,5 +12,5 @@ fi
 echo "Container type: ${CONTAINER_TYPE}"
 
 echo "Test executables"
-EXECUTABLES=('anaconda --version')
+EXECUTABLES=('conda --version')
 for EXE in "${EXECUTABLES[@]}"; do echo "Testing ${EXE}"; ${EXE}; if [ $? -ne 0 ]; then exit 1; fi; done
