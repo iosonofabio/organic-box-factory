5b8ab0533b7c65b3afa86bc4b5ee47fe9b1e6c52
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-05-09 17:20:19 -0700

    Remove cache more often to save space

--- configure_image.sh
+++ configure_image.sh
@@ -11,6 +11,8 @@ pacman -Syu --noconfirm
 echo 'Install pacman packages...'
 pacman --noconfirm -S ${PACMAN_PACKAGES[@]}
 echo 'pacman packages installed'
+# Remove pacman cache
+pacman -Scc --noconfirm
 
 # Prepare nonroot user for makepkg
 useradd -m -g users -G wheel -s /bin/bash singleceller
@@ -19,10 +21,10 @@ useradd -m -g users -G wheel -s /bin/bash singleceller
 cd /home/singleceller; mkdir -p packages/aura; cd packages/aura; wget https://aur.archlinux.org/cgit/aur.git/snapshot/aura-bin.tar.gz; tar -xvf aura-bin.tar.gz; cd aura-bin; chmod -R a+wrX /home/singleceller/packages/aura; su singleceller -c makepkg; pacman -U aura-bin-1.3.8-1-x86_64.pkg.tar --noconfirm
 
 # Install AUR packages
-for PKGNAME in ${AUR_PACKAGES[@]}; do cd /home/singleceller; mkdir -p packages/${PKGNAME}; cd packages/${PKGNAME}; aura -Aw ${PKGNAME}; tar -xf ${PKGNAME}.tar.gz; chmod -R a+wrX /home/singleceller/packages/${PKGNAME}; cd ${PKGNAME}; su singleceller -c makepkg; pacman -U $(ls "${PKGNAME}"-*.pkg.tar) --noconfirm; done
+for PKGNAME in ${AUR_PACKAGES[@]}; do cd /home/singleceller; mkdir -p packages/${PKGNAME}; cd packages/${PKGNAME}; aura -Aw ${PKGNAME}; tar -xf ${PKGNAME}.tar.gz; chmod -R a+wrX /home/singleceller/packages/${PKGNAME}; cd ${PKGNAME}; su singleceller -c makepkg; pacman -U $(ls "${PKGNAME}"-*.pkg.tar) --noconfirm; cd; rm -rf /home/singleceller/packages/${PKGNAME}; done
 
 # Install AUR packages with fixed and split assets
-for PKGNAME in ${AUR_PACKAGES_FIXED[@]}; do cd /home/singleceller; mkdir -p packages/${PKGNAME}; cd packages/${PKGNAME}; mkdir ${PKGNAME}; PKGVER=$(grep '^pkgver=' /assets/${PKGNAME}/PKGBUILD | sed 's/pkgver=//'); mv /assets/${PKGNAME}/PKGBUILD ${PKGNAME}/PKGBUILD; zcat /assets/${PKGNAME}/split/xa*.gz > ${PKGNAME}/${PKGNAME}-${PKGVER}.tar.gz && rm -rf /assets/${PKGNAME}; chmod -R a+wrX /home/singleceller/packages/${PKGNAME}; cd ${PKGNAME}; su singleceller -c makepkg; pacman -U $(ls "${PKGNAME}"-*.pkg.tar) --noconfirm; done
+for PKGNAME in ${AUR_PACKAGES_FIXED[@]}; do cd /home/singleceller; mkdir -p packages/${PKGNAME}; cd packages/${PKGNAME}; mkdir ${PKGNAME}; PKGVER=$(grep '^pkgver=' /assets/${PKGNAME}/PKGBUILD | sed 's/pkgver=//'); mv /assets/${PKGNAME}/PKGBUILD ${PKGNAME}/PKGBUILD; zcat /assets/${PKGNAME}/split/xa*.gz > ${PKGNAME}/${PKGNAME}-${PKGVER}.tar.gz && rm -rf /assets/${PKGNAME}; chmod -R a+wrX /home/singleceller/packages/${PKGNAME}; cd ${PKGNAME}; su singleceller -c makepkg; pacman -U $(ls "${PKGNAME}"-*.pkg.tar) --noconfirm; cd; rm -rf /home/singleceller/packages/${PKGNAME}; done
 
 # Install anaconda environments
 source /opt/anaconda/bin/activate root
