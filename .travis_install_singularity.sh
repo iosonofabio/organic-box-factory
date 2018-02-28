9a551bf50541c00dd4ee2eb7898d86ee6cea6b3c
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-02-27 17:36:21 -0800

    Update to newer version of singularity

--- .travis_install_singularity.sh
+++ .travis_install_singularity.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 pkgname='singularity-container'
-pkgver=2.2.1
+pkgver=2.4.2
 
 echo "Download"
 mkdir ${pkgname}
 cd ${pkgname}
-wget https://github.com/gmkurtzer/singularity/archive/${pkgver}.tar.gz
+wget https://github.com/singularityware/singularity/releases/download/${pkgver}/singularity-${pkgver}.tar.gz
 
 echo "Extract tar.gz"
 tar -xvf ${pkgver}.tar.gz
